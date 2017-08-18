080eda9100a6c359f0870584735f424e5f63b586
Leonie Weißweiler <30300891+LeonieWeissweiler@users.noreply.github.com>
2017-08-18 15:37:43 +0200

    First version of Cistem for Perl

---
+++ Cistem.pm
@@ -0,0 +1,154 @@
+#!/usr/bin/perl
+use utf8;
+package Cistem;
+
+sub stem{
+    $word = shift;
+    $case_insensitive = shift;
+
+    $word =~ s/Ü/U/g;
+    $word =~ s/Ö/O/g;
+    $word =~ s/Ä/A/g;
+
+    $word =~ s/üü/uu/g; #necessary because of Perl Unicode problems
+    $word =~ s/öö/oo/g;
+    $word =~ s/ää/aa/g;
+
+    $word =~ s/ü/u/g;
+    $word =~ s/ö/o/g;
+    $word =~ s/ä/a/g;
+
+    $upper = (ucfirst $word eq $word);
+
+    $word = lc($word);
+
+    $word =~ s/ß/ss/g;
+
+    $word =~ s/^ge(.{4,})/\1/;
+
+    $word =~s/sch/\$/g;
+
+    $word =~ s/(.)\1/\1\*/g;
+
+    $word =~s/ei/\%/g;
+    $word =~s/ie/\&/g;
+
+    while(length($word)>3){
+        if(length($word)>5 && ($word =~ s/e[mr]$// || $word =~ s/nd$//)){
+            pass;
+        }
+        elsif((!($upper) || $case_insensitive) && $word =~ s/t$//){
+            pass;
+        }
+        elsif($word =~ s/[esn]$//){
+            pass;
+        }
+        else{
+            last;
+        }
+    }
+
+    $word =~s/(.)\*/\1\1/g;
+
+    $word =~s/\$/sch/g;
+
+    $word =~s/\%/ei/g;
+    $word =~s/\&/ie/g;
+
+    return $word;
+}
+
+sub segment{
+    $word = shift;
+    $case_insensitive = shift;
+
+    $rest = "";
+
+    $upper = (ucfirst $word eq $word);
+
+    $word = lc($word);
+
+    $word =~s/sch/\$/g;
+
+    $word =~ s/(.)\1/\1\*/g;
+
+    $word =~s/ei/\%/g;
+    $word =~s/ie/\&/g;
+
+    while(length($word)>3){
+        if(length($word)>5 && ($word =~ s/(e[mr])$// || $word =~ s/(nd)$//)){
+            $rest = $1 . $rest;
+        }
+        elsif((!($upper) || $case_insensitive) && $word =~ s/t$//){
+            $rest = "t" . $rest;
+        }
+        elsif($word =~ s/([esn])$//){
+            $rest = $1 . $rest;
+        }
+        else{
+            last;
+        }
+    }
+
+    $word =~s/(.)\*/\1\1/g;
+
+    $word =~s/\$/sch/g;
+
+    $word =~s/\%/ei/g;
+    $word =~s/\&/ie/g;
+
+    return ($word,$rest);
+}
+
+1;
+
+=pod
+
+=head1 NAME
+
+CISTEM Stemmer for German
+
+=head1 SYNOPSIS
+
+    use Cistem;
+    my $stemmed_word = stem($word);
+
+    or, for segmentation:
+
+    my @segments = segment($word);
+
+=head1 DESCRIPTION
+
+This is the official Perl implementation of the CISTEM stemmer.
+It is based on the paper
+Leonie Weißweiler, Alexander Fraser (2017). Developing a Stemmer for German Based on a Comparative Analysis of Publicly Available Stemmers. In Proceedings of the German Society for Computational Linguistics and Language Technology (GSCL)
+which can be read here:
+http://www.cis.lmu.de/~weissweiler/cistem/
+
+In the paper, we conducted an analysis of publicly available stemmers, developed
+two gold standards for German stemming and evaluated the stemmers based on the
+two gold standards. We then proposed the stemmer implemented here and show
+that it achieves slightly better f-measure than the other stemmers and is
+thrice as fast as the Snowball stemmer for German while being about as fast as
+most other stemmers.
+
+=head1 METHODS
+
+=over 8
+
+=item stem($word, $case_insensitivity)
+
+This method takes the word to be stemmed and a boolean specifiying if case-insensitive stemming should be used and returns the stemmed word. If only the word
+is passed to the method or the second parameter is 1, normal case-sensitive stemming is used, if the second parameter is 0, case-insensitive stemming is used.
+
+Case sensitivity improves performance only if words in the text may be incorrectly upper case.
+For all-lowercase and correctly cased text, best performance is achieved by
+using the case-sensitive version.
+
+=item segment($word, $case_insensitivity)
+
+This method works very similarly to stem. The only difference is that in
+addition to returning the stem, it also returns the rest that was removed at
+the end. To be able to return the stem unchanged so the stem and the rest
+can be concatenated to form the original word, all subsitutions that altered
+the stem in any other way than by removing letters at the end were left out.
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CISTEM
-Stemmer for German
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
+
+CISTEM is a stemming algorithm for the german language, developed by Leonie Weißweiler and Alexander Fraser. This repository contains official implementations in a variety of programming languages. At the moment, Perl is the only language
+available, with Python and Java planned for the immediate future.
+
+## Performance
+We performed a comparative analysis of six publicly available German stemmers, where CISTEM achieved the best results for f-measure and state-of-the-art
+results for runtime.
+
+![](Images/Runtime.pdf)
+
+![](Images/F1.pdf)
+
+More information on how we evaluated runtimes and stemming quality can be found in our paper:
+
+![http://cis.lmu.de/~weissweiler/cistem/](https://cis.lmu.de/~weissweiler/cistem/)
